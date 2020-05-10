913db9401922c80e8762bf5da291a1b3145c2991
Berkhan <berkhanhalacoglu@hotmail.com>
2020-05-10 19:14:52 +0300

    C14: SocketIO Redis Adaptor

--- src/socketApi.js
+++ src/socketApi.js
@@ -6,6 +6,15 @@ const socketApi = {
     io
 };
 
+/*
+*Redis adaptor. Eğer birden fazla server varsa socketlerin biriri ile senkronize olması sağlanır.
+*/
+
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT}));
+
 io.on('connection', socket=>{
     console.log('Bir kullanıcı bağlandı.');
 });
